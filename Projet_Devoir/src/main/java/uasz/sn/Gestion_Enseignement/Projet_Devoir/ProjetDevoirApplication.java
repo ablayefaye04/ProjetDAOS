019b37d012f18250522eb8bd0f0d82daeed6a942
ablayefaye04 <fayeablaye404@gmail.com>
2025-01-31 22:45:09 +0000

    l'ajote du projet devoir daos sur github

---
+++ Projet_Devoir/src/main/java/uasz/sn/Gestion_Enseignement/Projet_Devoir/ProjetDevoirApplication.java
@@ -0,0 +1,13 @@
+package uasz.sn.Gestion_Enseignement.Projet_Devoir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetDevoirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetDevoirApplication.class, args);
+	}
+
+}
---
+++ Projet_Devoir/src/test/java/uasz/sn/Gestion_Enseignement/Projet_Devoir/ProjetDevoirApplicationTests.java
@@ -0,0 +1,13 @@
+package uasz.sn.Gestion_Enseignement.Projet_Devoir;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjetDevoirApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
